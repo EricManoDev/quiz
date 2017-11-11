7f39c1861e69d98e7b4a829e4ff70a98741bfc2d
Formation <Formation@10.18.0.65>
2017-11-11 13:08:20 +0100

    V 1.0

--- src/quiz/PointEntree.java
+++ src/quiz/PointEntree.java
@@ -5,6 +5,8 @@
  */
 package quiz;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Formation
@@ -15,7 +17,44 @@ public class PointEntree {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
+        boolean quitter = false;
+        while (quitter == false) {
+
+            System.out.println("MENU PRINCIPAL");
+            System.out.println("----------------");
+            System.out.println("1. Ajouter nouveau Quiz");
+            System.out.println("2. Jouer au Quiz");
+            System.out.println("3. Quitter");
+            System.out.print("votre choix: ");
+
+            // Saisie Clavier
+            Scanner scanner = new Scanner(System.in);
+            String select = scanner.nextLine();
+            switch (select) {
+                case "1":
+                    ajouterNouveauQuiz();
+                    break;
+                case "2":
+                    jouerQuiz();
+                    break;
+                case "3":
+                    quitter = true;
+                    break;
+                default:
+                    System.out.println("Sélectionner à nouveau");
+
+            }
+
+        }
+
     }
-    
+
+    private static void ajouterNouveauQuiz() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void jouerQuiz() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
---
+++ src/quiz/Quiz.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Formation
+ */
+public class Quiz {
+    
+          Quiz q1 = new Quiz();
+        q1.jouerQuiz();
+
+    /////////Jouer Quiz///////////
+            public void jouerQuiz() {
+            
+
+    }
+
+    
+
+}
